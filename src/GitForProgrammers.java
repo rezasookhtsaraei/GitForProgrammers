3469a074fcd871b01bb5a745d5fd378391798f0a
reza sookhtsaraei <reza.sookhtsaraei@gmail.com>
2023-04-14 16:33:42 +0430

    println in single line

--- src/GitForProgrammers.java
+++ src/GitForProgrammers.java
@@ -1,6 +1,5 @@
 public class GitForProgrammers {
     public static void main(String[] args) {
-        System.out.println("Welcome to git");
-        System.out.println("For Learning");
+        System.out.println("Welcome to git for learn");
     }
 }
